fffe6e7dd34016325603993fd8272cf1dfd91743
rajesh2126 <rajesh.robin21@gmail.com>
2024-08-25 23:01:52 +0530

    first commit

---
+++ src/page/HomePage.java
@@ -0,0 +1,11 @@
+package page;
+
+public class HomePage {
+	
+	
+	public static void main(String a[]) {
+
+		System.out.println("Hello HP");
+	}
+
+}
---
+++ src/page/LoginPage.java
@@ -0,0 +1,10 @@
+package page;
+
+public class LoginPage {
+	
+	public static void main (String a[]) {
+		
+		System.out.println("Hello LP");
+	}
+
+}
